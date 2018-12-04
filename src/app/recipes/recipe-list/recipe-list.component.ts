018c7b58c205fb4f2d9b69d8783acba6aaba9d17
Heinitz, Michael <Michael.Heinitz@adidas.com>
2018-12-04 20:17:44 +0100

    Before service section

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe(
+      "Test Recipe",
+      "This is simply a test!",
+      "https://www.maxpixel.net/static/photo/2x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg"
+    ),
+    new Recipe(
+      "Test Recipe2",
+      "This is the second test!",
+      "https://www.maxpixel.net/static/photo/2x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg"
+    )
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+}
